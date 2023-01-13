f4231945313dd09441db27b89c70e439df791112
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-01-13 16:06:48 -0300

    continue to set up for winner

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,8 +15,8 @@ const Game = () => {
   const [h9, seth9] = useState("");
   const [winCondition, setWinCondition] = useState("");
   const [counter, setCounter] = useState(0);
-  const [haveWinner, setHavewinner] = useState('');
-  const [initialGame, seInitialGame] = useState('');
+  const [haveWinner, setHavewinner] = useState(false);
+  const [initialGame, seInitialGame] = useState("");
 
   const handlePlayer = () => {
     setPlayer(player * -1);
@@ -24,7 +24,7 @@ const Game = () => {
 
   const handleSetCounter = () => {
     setCounter(counter + 1);
-  }
+  };
 
   useEffect(() => {
     if (
@@ -33,61 +33,60 @@ const Game = () => {
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
+    }
     if (
       (h2 === "X" && h5 === "X" && h8 === "X") ||
       (h2 === "O" && h5 === "O" && h8 === "O")
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
+    }
     if (
       (h3 === "X" && h6 === "X" && h9 === "X") ||
       (h3 === "O" && h6 === "O" && h9 === "O")
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
+    }
     if (
       (h1 === "X" && h2 === "X" && h3 === "X") ||
       (h1 === "O" && h2 === "O" && h3 === "O")
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
+    }
     if (
       (h4 === "X" && h5 === "X" && h6 === "X") ||
       (h4 === "O" && h5 === "O" && h6 === "O")
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
+    }
     if (
       (h7 === "X" && h8 === "X" && h9 === "X") ||
       (h7 === "O" && h8 === "O" && h9 === "O")
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
+    }
     if (
       (h1 === "X" && h5 === "X" && h9 === "X") ||
       (h1 === "O" && h5 === "O" && h9 === "O")
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
+    }
     if (
       (h3 === "X" && h5 === "X" && h7 === "X") ||
       (h3 === "O" && h5 === "O" && h7 === "O")
     ) {
       console.log("Ganhou");
       setHavewinner(true);
-    };
-    if(counter === 9){
-      console.log('Game over')
+    }
+    if (counter === 9 && haveWinner) {
+      console.log("Game over");
     }
   }, [h1, h2, h3, h4, h5, h6, h7, h8, h9, counter]);
-
 
   const handleH1 = () => {
     if (h1 === "") {
@@ -201,35 +200,35 @@ const Game = () => {
     <div>
       <h1>Jogo da Velha</h1>
       <div className="row">
-        <button className="btn-game" type="submit" onClick={handleH1}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH1}>
           <span>{h1}</span>
         </button>
-        <button className="btn-game" type="submit" onClick={handleH2}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH2}>
           {h2}
         </button>
-        <button className="btn-game" type="submit" onClick={handleH3}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH3}>
           {h3}
         </button>
       </div>
       <div className="row">
-        <button className="btn-game" type="submit" onClick={handleH4}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH4}>
           {h4}
         </button>
-        <button className="btn-game" type="submit" onClick={handleH5}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH5}>
           {h5}
         </button>
-        <button className="btn-game" type="submit" onClick={handleH6}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH6}>
           {h6}
         </button>
       </div>
       <div className="row">
-        <button className="btn-game" type="submit" onClick={handleH7}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH7}>
           {h7}
         </button>
-        <button className="btn-game" type="submit" onClick={handleH8}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH8}>
           {h8}
         </button>
-        <button className="btn-game" type="submit" onClick={handleH9}>
+        <button className="btn-game" type="submit" onClick={haveWinner ? '' :handleH9}>
           {h9}
         </button>
       </div>
